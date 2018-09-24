b899c1eaf0e4eb00606a277c50097733fc726b8b
Eric Zorn <ericzorn@erics-mbp-2018.home>
2018-09-23 23:27:25 -0400

    Audio Player working with global variable

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -13,6 +13,7 @@ import AVFoundation
 class ViewController: UIViewController{
     
     var audioPlayer = AVAudioPlayer()
+    var selectedSoundFileName : String = ""
     let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
@@ -21,7 +22,7 @@ class ViewController: UIViewController{
 
     
     @IBAction func notePressed(_ sender: UIButton) {
-        var selectedSoundFileName : String = soundArray[sender.tag - 1]
+        selectedSoundFileName = soundArray[sender.tag - 1]
         print(selectedSoundFileName)
         
         playSound()
@@ -29,7 +30,7 @@ class ViewController: UIViewController{
     
     
     func playSound() {
-        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
